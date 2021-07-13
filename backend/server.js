dadb95f7065a5ce47ef2c0f8ac8baf07d6cabb33
saish gadekar <saish.gadekar.58@gmail.com>
2021-07-14 02:09:20 +0530

    1st

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import data from "./data.js";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+app.listen(port, () => {
+  console.log(`running at ${port}`);
+});
