65db8364fea05b5549d56da083ecae7c72ad8879
saish gadekar <saish.gadekar.58@gmail.com>
2021-07-14 21:42:59 +0530

    afterreduxtosingleproduct

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 5000;
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  }
+  res.status(404).send({ message: "product not found" });
+});
 
 app.get("/", (req, res) => {
   res.send("server is ready");
