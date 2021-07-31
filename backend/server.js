367836e0dc13de4acb81e1037917a5262ee2b5ff
saish gadekar <saish.gadekar.58@gmail.com>
2021-08-01 01:46:24 +0530

    orderScreen

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -25,6 +26,7 @@ const port = process.env.PORT || 5000;
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
   next();
