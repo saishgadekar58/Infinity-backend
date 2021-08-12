7cf67c80974bc38042402af1ecba3959558bea32
saish gadekar <saish.gadekar.58@gmail.com>
2021-08-12 22:32:53 +0530

    payment

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,12 @@ mongoose
     console.error(error);
   });
 const port = process.env.PORT || 5000;
-
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
   next();
