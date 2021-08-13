840675f01a2bbf23acad41cd2eb4cce02db9ccb6
saish gadekar <saish.gadekar.58@gmail.com>
2021-08-13 23:35:31 +0530

    heroku

--- backend/server.js
+++ backend/server.js
@@ -36,10 +36,11 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // app.use(express.static(path.join("client/build")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
+  app.use(express.dtatic("/client/build"));
 }
 
 app.listen(port, () => {
