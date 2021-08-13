8c57304d2fa10fc77a625abf4c5b0a1ddf721734
saish gadekar <saish.gadekar.58@gmail.com>
2021-08-14 00:18:45 +0530

    t4

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ mongoose
 const port = process.env.PORT;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join("client/build")));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
   // app.use(express.static("/client/build"));
