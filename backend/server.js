80b6dbd6260da5a6b3cb0958110ec733828e1919
saish gadekar <saish.gadekar.58@gmail.com>
2021-08-14 01:16:21 +0530

    t9

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import path from "path";
-const path = require("path");
+import path from "path";
+// const path = require("path");
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
   // app.use(express.static("/client/build"));
 } else {
